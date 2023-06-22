2533b5f3cb2133a34f93bd2e7b63aaea0fe3b5b1
JairoCardenas123 <CardenasJairo102011@gmail.com>
2023-06-22 11:48:57 -0500

    fix: SAD

--- server.js
+++ server.js
@@ -1,14 +1,34 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req,res)=>{
      console.log(req.url,req.method,req.headers);
+     const url = req.url;
+     const method = req.method
+
+
+     if(url === '/'){
+     res.write('<html>');
+     res.write('<head><title>My First response page</title></head>');
+     res.write('<body><form action = "/message" method = "POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+     res.write('</html>');
+     return res.end();
+     }
+
+     if(url === '/message' && method === 'POST'){
+          fs.writeFileSync('message.txt','dudeee');
+          res.statusCode = 302;
+          res.setHeader('Location','/');
+          return res.end();
+     } 
 
      res.setHeader('Content-Type','text/html');
      res.write('<html>');
-     res.write('<head><title>My first response page</title></head>')
-     res.write('<body><h1>Hello from node js</h1></body>')
+     res.write('<head><title>My First response page</title></head>');
+     res.write('<body><h1>Hello fron node JS DUDEEE</h1></body>');
      res.write('</html>');
-     res.end;
+     res.end();
+
 });
 
-server.listen(5000);
+server.listen();
